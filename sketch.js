f994a3e62be35c6d4d8117a827b1809873ce954f
FlorianLUSSON <florianlusson@gmail.com>
2023-03-20 16:24:29 +0100

    Horloge OK

--- sketch.js
+++ sketch.js
@@ -1,39 +1,77 @@
-let y;
+let cx, cy;
+let secondsRadius;
+let minutesRadius;
+let hoursRadius;
+let clockDiameter;
 
-// The statements in the setup() function
-// execute once when the program begins
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   stroke(255);
-  noLoop();
-  y = height * 0.5;
-  x = width / 2;
+
+  let radius = min(200, 200) / 2;
+  secondsRadius = radius * 0.71;
+  minutesRadius = radius * 0.6;
+  hoursRadius = radius * 0.5;
+  clockDiameter = radius * 1.7;
+
+  cx = width-150
+  cy = 150;
+
+  dx = width - 230
+  dy = 300
+  
 }
 
-// The statements in draw() are executed until the
-// program is stopped. Each statement is executed in
-// sequence and after the last line is read, the first
-// line is executed again. 
 function draw() {
-  background(0);
-  x = x - 4;
-  y = y - 4;
-  if(x < 0){
-    x=width
-  }
-  if (y < 0) {
-    y = height;
-  }
-  line(x,0,x,height)
-  line(0, y, width, y);
-  setTimeout(draw,1000);
+  background(0,75,120);
+  drawClock()
+  drawDigitalClock()
 }
 
-function calcul(){
-  let calcul = 0
-  for(i=0;i<10;i++){
-    calcul += 2
-    console.log(calcul)
-  }
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight)
 }
-calcul()
+
+function drawDigitalClock(){
+  let heure = hour()
+  let min = minute()
+  let sec = second()
+  textSize(40)
+  text(heure + ":"+min+":"+sec,dx,dy)
+}
+
+function drawClock(){
+  
+  // Draw the clock background
+  noStroke();
+  fill(244, 122, 158);
+  ellipse(cx, cy, clockDiameter + 25, clockDiameter + 25);
+  fill(237, 34, 93);
+  ellipse(cx, cy, clockDiameter, clockDiameter);
+
+  // Angles for sin() and cos() start at 3 o'clock;
+  // subtract HALF_PI to make them start at the top
+  let s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
+  let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
+  let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
+
+  // Draw the hands of the clock
+  stroke(255);
+  strokeWeight(1);
+  line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
+  strokeWeight(2);
+  line(cx, cy, cx + cos(m) * minutesRadius, cy + sin(m) * minutesRadius);
+  strokeWeight(4);
+  line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
+
+  // Draw the minute ticks
+  strokeWeight(2);
+  beginShape(POINTS);
+  for (let a = 0; a < 360; a += 6) {
+    let angle = radians(a);
+    let x = cx + cos(angle) * secondsRadius;
+    let y = cy + sin(angle) * secondsRadius;
+    vertex(x, y);
+  }
+  endShape();
+}
